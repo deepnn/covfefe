4fd9b1ab7ea9946c74c048aaaf20e7d090eba46e
esube <esube.tamirat@gmail.com>
2016-10-01 22:13:09 -0400

    added lasagne layers imports

--- coffee/frameworks/theano/layers.py
+++ coffee/frameworks/theano/layers.py
@@ -1,3 +1,12 @@
+from __future__ import absolute_import
+
+from lasagne.layers import get_all_layers
+from lasagne.layers import get_output
+from lasagne.layers import InputLayer
+from lasagne.layers import Layer
+
+from lasagne.layers import Conv2DLayer
+from lasagne.layers import MaxPool2DLayer
 
 # core: Data, Dropout, Reshape, Permute, RepeatVevtor, Dense
 
