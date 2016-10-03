932a87cc13248cf3d67c290614d0af5b52bb9275
esube <esube.tamirat@gmail.com>
2016-10-02 22:24:44 -0400

    added caffe framework initial imports

---
+++ coffee/frameworks/caffe/activations.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+from caffe.net_spec import NetSpec, to_proto
+
+from caffe import layers as L
+from caffe import params as P
---
+++ coffee/frameworks/caffe/optimizations.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+from caffe.pycaffe import Net, 
+                    SGDSolver, NesterovSolver, 
+                    AdaGradSolver, RMSPropSolver, 
+                    AdaDeltaSolver, AdamSolver
