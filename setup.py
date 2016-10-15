f462d5167ce18a6c96c71d183f93cb4f50bcec2c
esube <esube.tamirat@gmail.com>
2016-10-14 23:03:20 -0400

    another doc fix to incorporate mdx math latex rendering

--- setup.py
+++ setup.py
@@ -1,42 +1,26 @@
-from setuptools import setup
-from setuptools import find_packages
+#!/usr/bin/env python3
 
+from distutils.core import setup
 
-setup(name='coffee',
-      version='0.0.1.alpha',
-      description='Python wrapper for major deep learning frameworks',
-      author='coffee contributers',
-      author_email='esube.tamirat@gmail.com',
-      url='https://github.com/deepnn/coffee',
-      download_url='https://github.com/deepnn/coffee/tarball/0.0.0.alpha',
-      license='MIT',
-      install_requires=[
-          'theano', 
-          'pyyaml', 
-          'six',
-          'caffe',
-          'lasagne',
-          'tflearn'
-      ],
-      extras_require={
-          'h5py': ['h5py'],
-          'visualize': ['pydot-ng'],
-      },
-      classifiers=[
-          'Programming Language :: Python',
-          'Operating System :: Platform Independent',
-          'Intended Audience :: Developers'
-          'Intended Audience :: Science/Research',
-          'Topic :: Scientific/Engineering :: Artificial Intelligence'
-      ],
-      keywords=[
-          'Coffee',
-          'Caffe',
-          'Theano',
-          'TensorFlow',
-          'Deep Learning',
-          'Machine Learning',
-          'Neural Networks',
-          'AI'
-      ]
-      packages=find_packages())
+long_description = \
+"""This extension adds math formulas support to Python-Markdown_
+(works with version 2.6 or newer).
+
+.. _Python-Markdown: https://github.com/waylan/Python-Markdown
+
+You can find the source on GitHub_.
+Please refer to the `README file`_ for details on how to use it.
+
+.. _GitHub: https://github.com/mitya57/python-markdown-math
+.. _`README file`: https://github.com/mitya57/python-markdown-math/blob/master/README.md
+"""
+
+setup(name='python-markdown-math',
+      description='Math extension for Python-Markdown',
+      long_description=long_description,
+      author='Dmitry Shachnev',
+      author_email='mitya57@gmail.com',
+      version='0.2',
+      url='https://github.com/mitya57/python-markdown-math',
+      py_modules=['mdx_math'],
+      license='BSD')
