4e8535457db60ad176899e15ff3f1cb968ce9fde
esube <esube.tamirat@gmail.com>
2017-03-26 13:21:58 -0400

    fixed the calls to return the layer objects instead

--- coffee/frameworks/torch/activations.py
+++ coffee/frameworks/torch/activations.py
@@ -6,60 +6,60 @@ Activation functions common API wrappers for torch frameowrk.
 import torch.nn as nn
 
 # sigmoid
-def sigmoid(x):
-    return nn.Sigmoid()(x)
+def sigmoid():
+    return nn.Sigmoid()
 
-def log_sigmoid(x):
-    return nn.LogSigmoid()(x)
+def log_sigmoid():
+    return nn.LogSigmoid()
    
 # tanh
-def tanh(x):
-    return nn.Tanh()(x)
+def tanh():
+    return nn.Tanh()
 
-def hard_tanh(x, min_val=-1, max_val=1, inplace=False):
-    return nn.Hardtanh(min_value=min_val, max_value=max_val, inplace=inplace)(x)
+def hard_tanh(min_val=-1, max_val=1, inplace=False):
+    return nn.Hardtanh(min_value=min_val, max_value=max_val, inplace=inplace)
 
-def tanh_shrink(x):
-    return nn.Tanhshrink()(x)
+def tanh_shrink():
+    return nn.Tanhshrink()
 
 # rectify
-def relu(x, relu6=False, inplace=False):
+def relu(relu6=False, inplace=False):
     if relu6 == True:
-        return nn.ReLU6(inplace=inplace)(x)
+        return nn.ReLU6(inplace=inplace)
     else:
-        return nn.ReLU(inplace=inplace)(x)
+        return nn.ReLU(inplace=inplace)
 
-def leaky_relu(x, neg_slope=0.01, inplace=False):
-    return nn.LeakyReLU(negative_slope=neg_slope, inplace=inplace)(x)
+def leaky_relu(neg_slope=0.01, inplace=False):
+    return nn.LeakyReLU(negative_slope=neg_slope, inplace=inplace)
 
-def elu(x, alpha=1.0, inplace=False):
-    return nn.ELU(alpha=alpha, inplace=inplace)(x)
+def elu(alpha=1.0, inplace=False):
+    return nn.ELU(alpha=alpha, inplace=inplace)
 
-def prelu(x, num_param=1, init=0.25):
-    return nn.PReLU(num_parameters=num_param, init=init)(x)
+def prelu(num_param=1, init=0.25):
+    return nn.PReLU(num_parameters=num_param, init=init)
 
 # soft
-def softplus(x, beta=1, th=20):
-    return nn.Softplus(beta=beta, threshold=th)(x)
+def softplus(beta=1, th=20):
+    return nn.Softplus(beta=beta, threshold=th)
 
-def softmin(x):
-    return nn.Softmin()(x)
+def softmin():
+    return nn.Softmin()
 
-def softmax(x):
-    return nn.Softmax()(x)
+def softmax():
+    return nn.Softmax()
 
-def log_softmax(x):
-    return nn.LogSoftmax()(x)
+def log_softmax():
+    return nn.LogSoftmax()
 
-def soft_shrink(x, lam=0.5):
-    return nn.Softshrink(lambd=lam)(x)
+def soft_shrink(lam=0.5):
+    return nn.Softshrink(lambd=lam)
 
-def soft_sign(x):
-    return nn.Softsign()(x)
+def soft_sign():
+    return nn.Softsign()
 
 # threshold
-def threshold(x, th, val, inplace=False):
-    return nn.Threshold(threshold=th, value=val, inplace=inplace)(x)
+def threshold(th, val, inplace=False):
+    return nn.Threshold(threshold=th, value=val, inplace=inplace)
 
 # linear
 def linear(x):
--- coffee/frameworks/torch/layers.py
+++ coffee/frameworks/torch/layers.py
@@ -5,28 +5,28 @@ import torch.nn as nn
 # core: Dense, Dropout, Convolutional
 
 # Dense (Linear)
-def dense(x, in_size, out_size, bias=True):
+def dense(in_size, out_size, bias=True):
     return nn.Linear(in_size=in_size,
                     out_size=out_size,
-                    bias=bias)(x)
+                    bias=bias)
 
 # Dropout
-def dropout(x, p=0.5, inplace=False):
+def dropout(p=0.5, inplace=False):
     
     #TODO: in the future some preprocessing goes here
     in_dim = x.dim()
     if in_dim == 1:
-        return nn.Dropout(p=p, inplace=inplace)(x)
+        return nn.Dropout(p=p, inplace=inplace)
 
     elif in_dim == 2:
-        return nn.Dropout2d(p=p, inplace=inplace)(x)
+        return nn.Dropout2d(p=p, inplace=inplace)
 
     elif in_dim == 3:
-        return nn.Dropout3d(p=p, inplace=inplace)(x)
+        return nn.Dropout3d(p=p, inplace=inplace)
 
 # convolutional
 # Regular convolution
-def conv(x, in_ch, out_ch, kernel_size,
+def conv(in_ch, out_ch, kernel_size,
             stride=1, padding=0, dilation=1, groups=1, bias=True):
     
     #TODO: in the future some preprocessing goes here
@@ -37,7 +37,7 @@ def conv(x, in_ch, out_ch, kernel_size,
                         padding=padding,
                         dilation=dilation,
                         groups=groups,
-                        bias=bias)(x)
+                        bias=bias)
 
     elif in_dim == 2:
         return nn.Conv2d(in_ch, out_ch, kernel_size,
@@ -45,7 +45,7 @@ def conv(x, in_ch, out_ch, kernel_size,
                         padding=padding,
                         dilation=dilation,
                         groups=groups,
-                        bias=bias)(x)
+                        bias=bias)
 
     elif in_dim == 3:
         return nn.Conv3d(in_ch, out_ch, kernel_size,
@@ -53,9 +53,9 @@ def conv(x, in_ch, out_ch, kernel_size,
                         padding=padding,
                         dilation=dilation,
                         groups=groups,
-                        bias=bias)(x)
+                        bias=bias)
 # Transposed Concolution
-def conv_transpose(x, in_ch, out_ch, kernel_size,
+def conv_transpose(in_ch, out_ch, kernel_size,
                       stride=1, padding=0, out_padding=0, 
                       dilation=1, groups=1, bias=True):
     
@@ -68,7 +68,7 @@ def conv_transpose(x, in_ch, out_ch, kernel_size,
                         output_padding=out_padding,
                         dilation=dilation,
                         groups=groups,
-                        bias=bias)(x)
+                        bias=bias)
 
     elif in_dim == 2:
         return nn.ConvTranspose2d(in_ch, out_ch, kernel_size,
@@ -77,7 +77,7 @@ def conv_transpose(x, in_ch, out_ch, kernel_size,
                         output_padding=out_padding,
                         dilation=dilation,
                         groups=groups,
-                        bias=bias)(x)
+                        bias=bias)
 
     elif in_dim == 3:
         return nn.ConvTranspose3d(in_ch, out_ch, kernel_size,
@@ -86,10 +86,10 @@ def conv_transpose(x, in_ch, out_ch, kernel_size,
                         output_padding=out_padding,
                         dilation=dilation,
                         groups=groups,
-                        bias=bias)(x)
+                        bias=bias)
 
 # pooling
-def pool(x, kernel_size, power=2, output_size=None, 
+def pool(kernel_size, power=2, output_size=None, 
             out_ratio=None, stride=None, padding=0, 
             dilation=1, return_indices=False, ceil_mode=False, 
             mode='max', count_include_pad=True, _random_samples=None):
@@ -100,63 +100,63 @@ def pool(x, kernel_size, power=2, output_size=None,
             return nn.MaxPool1d(kernel_size=kernel_size, stride=stride, 
                         padding=padding, dilation=dilation, 
                         return_indices=return_indices, 
-                        ceil_mode=ceil_mode)(x)
+                        ceil_mode=ceil_mode)
 
         elif in_dim == 2:
             return nn.MaxPool2d(kernel_size=kernel_size, stride=stride, 
                         padding=padding, dilation=dilation, 
                         return_indices=return_indices, 
-                        ceil_mode=ceil_mode)(x)
+                        ceil_mode=ceil_mode)
 
         elif in_dim == 3:
             return nn.MaxPool3d(kernel_size=kernel_size, stride=stride, 
                         padding=padding, dilation=dilation, 
                         return_indices=return_indices, 
-                        ceil_mode=ceil_mode)(x)
+                        ceil_mode=ceil_mode)
 
     elif mode=='ave':
         if in_dim == 1:
             return nn.AvgPool1d(kernel_size=kernel_size, stride=stride, 
                         padding=padding, ceil_mode=ceil_mode, 
-                        count_include_pad=count_include_pad)(x)
+                        count_include_pad=count_include_pad)
 
         elif in_dim == 2:
             return nn.AvgPool2d(kernel_size=kernel_size, stride=stride, 
                         padding=padding, ceil_mode=ceil_mode, 
-                        count_include_pad=count_include_pad)(x)
+                        count_include_pad=count_include_pad)
 
         elif in_dim == 3:
-            return nn.AvgPool3d(kernel_size=kernel_size, stride=stride)(x)
+            return nn.AvgPool3d(kernel_size=kernel_size, stride=stride)
 
     elif mode=='fractional_max':
         return nn.FractionalMaxPool2d(kernel_size=kernel_size, 
                         output_size=out_size, 
                         output_ratio=out_ratio, 
                         return_indices=return_indices, 
-                        _random_samples=_random_samples)(x)
+                        _random_samples=_random_samples)
 
     elif mode=='power':
         return nn.LPPool2d(norm_type=power, kernel_size=kernel_size, 
-                        stride=stride, ceil_mode=ceil_mode)(x)
+                        stride=stride, ceil_mode=ceil_mode)
 
 # normalization
-def batch_norm(x, num_features, eps=1e-05, momentum=0.1, affine=True):
+def batch_norm(num_features, eps=1e-05, momentum=0.1, affine=True):
     
     in_dim = x.dim()
     if in_dim == 1:
         return nn.BatchNorm1d(num_features=num_features, 
                         eps=eps, 
                         momentum=momentum, 
-                        affine=affine)(x)
+                        affine=affine)
 
     elif in_dim == 2:
         return nn.BatchNorm2d(num_features=num_features, 
                         eps=eps, 
                         momentum=momentum, 
-                        affine=affine)(x)
+                        affine=affine)
 
     elif in_dim == 3:
         return nn.BatchNorm3d(num_features=num_features, 
                         eps=eps, 
                         momentum=momentum, 
-                        affine=affine)(x)
+                        affine=affine)
