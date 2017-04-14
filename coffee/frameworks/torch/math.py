bdc9c5593d85540cd26e6029bad3c72eae5656d4
esube <esube.tamirat@gmail.com>
2017-04-14 18:16:38 -0400

    updated tensor and math

--- coffee/frameworks/torch/math.py
+++ coffee/frameworks/torch/math.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+# This will make all the math functions of torch available
 from torch import *
--- coffee/frameworks/torch/tensor.py
+++ coffee/frameworks/torch/tensor.py
@@ -1,4 +1,24 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-from torch import *
+import torch as T
+import torch.autograd as G
+
+def tensor(*args, dtype='float'):
+    '''
+        Note: all types are not implemented yet
+    '''
+    if dtype == 'float':
+        return T.FloatTensor(args)
+    elif dtype == 'double':
+        return T.DoubleTensor(args)
+    elif dtype == 'int':
+        return T.IntTensor(args)
+    elif dtype == 'long':
+        return T.LongTensor(args)
+
+def variable(*args, dtype='float', grad=True):
+    '''
+        returns a variable that wraps the input tensor of dtype
+    '''
+    return G.Variable(tensor(args, dtype), requires_grad=grad)
