49c96585716fca6a01a34d52978725f7e07dce42
esube <esube.tamirat@gmail.com>
2017-04-14 18:30:15 -0400

    utils

--- coffee/frameworks/torch/utils.py
+++ coffee/frameworks/torch/utils.py
@@ -1,4 +1,16 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-from torch import *
+from torch.nn as nn
+
+def clip_grad(params, max_norm, norm_type=2):
+    return nn.utils.clip_grad_norm(params, max_norm, norm_type)
+
+def parallel(module, device_ids=None, output_device=None, dim=0):
+    '''
+        Parallelizes the module into the device_ids and
+        puts the output on the output_device
+    '''
+
+    return torch.nn.DataParallel(module, device_ids,
+                                 output_devie, dim)
